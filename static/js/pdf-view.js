d8181271483c71526204ff2fee556cfe4d2b6389
Charles Samuel R <rcharles.samuel99@gmail.com>
2022-09-22 20:17:55 +0530

    Changed PDF API Key

--- static/js/pdf-view.js
+++ static/js/pdf-view.js
@@ -1,5 +1,5 @@
 document.addEventListener("adobe_dc_view_sdk.ready", function(){ 
-    var adobeDCView = new AdobeDC.View({clientId: "ebad89bcc9ed40b1b1bb30cf65507969", divId: "adobe-dc-view"});
+    var adobeDCView = new AdobeDC.View({clientId: "16afbd9ba10346489c311b3f7d6dad72", divId: "adobe-dc-view"});
     adobeDCView.previewFile({
         content:{location: {url: "static/APB.pdf"}},
         metaData:{fileName: "APB.pdf"}
