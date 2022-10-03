4beaade6c5b16b792a053cd27205afee9c648188
Charles Samuel R <rcharles.samuel99@gmail.com>
2022-10-03 18:21:26 +0530

    Got rid of Adobe PDF Viewer

--- static/js/pdf-view.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("adobe_dc_view_sdk.ready", function(){ 
-    var adobeDCView = new AdobeDC.View({clientId: "16afbd9ba10346489c311b3f7d6dad72", divId: "adobe-dc-view"});
-    adobeDCView.previewFile({
-        content:{location: {url: "static/APB.pdf"}},
-        metaData:{fileName: "APB.pdf"}
-    }, {embedMode: "IN_LINE"});
-});
